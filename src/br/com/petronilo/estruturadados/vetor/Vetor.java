0d6e010dbe83c1e95dfd3d0efcad62e808781a1c
nilopadilha <nilopadilha@gmail.co>
2023-05-17 16:34:41 -0300

    segundo commit do curso de estrutura de dados

--- src/br/com/petronilo/estruturadados/vetor/Vetor.java
+++ src/br/com/petronilo/estruturadados/vetor/Vetor.java
@@ -1,6 +1,15 @@
 package br.com.petronilo.estruturadados.vetor;
 
 public class Vetor {
+
+	/*
+	 * por padrão quando se instancia um vetor de string no java, todos os elementos
+	 * teraõ valor null, que é o valor inicial de uma string do java. caso seja um
+	 * vetor de Boolean o valor inicial será false. caso seja um vetor de inteiro ou
+	 * qualque tipo primitivo que represente número no java o valor inicial será
+	 * zero.
+	 * 
+	 */
 	private String[] elementos;
 
 	public Vetor(int capacidade) {
@@ -8,6 +17,13 @@ public class Vetor {
 		this.elementos = new String[capacidade];
 	}
 
-	
+	public void adicionaElemento(String elemento) {
+		for (int i = 0; i < this.elementos.length; i++) {
+			if(this.elementos[i]== null) {
+				this.elementos[i] = elemento;
+				break;
+			}
+		}
+	}
 
 }
---
+++ src/br/com/petronilo/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,15 @@
+package br.com.petronilo.estruturadados.vetor.teste;
+
+import br.com.petronilo.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(5);
+		vetor.adicionaElemento("elemento 1");
+		vetor.adicionaElemento("elemento 2");
+
+	}
+
+}
