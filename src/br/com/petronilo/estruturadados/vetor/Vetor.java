a9e490a1915fc45233a196145a67ed1824bb50d8
nilopadilha <nilopadilha@gmail.co>
2023-05-17 15:48:27 -0300

    primeiro commit do curso de estrutura de dados

---
+++ src/br/com/petronilo/estruturadados/vetor/Vetor.java
@@ -0,0 +1,13 @@
+package br.com.petronilo.estruturadados.vetor;
+
+public class Vetor {
+	private String[] elementos;
+
+	public Vetor(int capacidade) {
+		super();
+		this.elementos = new String[capacidade];
+	}
+
+	
+
+}
---
+++ src/br/com/petronilo/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,15 @@
+package br.com.petronilo.estruturadados.vetor.teste;
+
+import br.com.petronilo.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+		Vetor vetor = new Vetor(5);
+		
+		int i=0;
+		i++;
+
+	}
+
+}
